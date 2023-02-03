34ad96d732b2aaf9c7f73650276bb73ed20c88b6
Mohamad Salimi <contact@salimi.my>
2023-02-03 15:04:55 +0800

    Update share

--- application/controllers/Share.php
+++ application/controllers/Share.php
@@ -1,30 +1,34 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Share extends CI_Controller {
+class Share extends CI_Controller
+{
 
-	function __construct() {
-		parent::__construct();
+  function __construct()
+  {
+    parent::__construct();
     $this->load->library('form_validation');
     $this->load->model('share_model');
   }
 
-	public function index(){
-		redirect('admin/login');
-	}
+  public function index()
+  {
+    redirect('admin/login');
+  }
 
-	/*
+  /*
   * Show file
   */
-  public function file($hash){
+  public function file($hash)
+  {
     // redirect admin login if hash empty
-    if(empty($hash)){
+    if (empty($hash)) {
       redirect('admin/login');
     }
 
-		$data = array();
+    $data = array();
 
-    if($this->input->post('form_submitted')){
+    if ($this->input->post('form_submitted')) {
       //form field validation rules
       $this->form_validation->set_rules('password', 'password', 'required');
 
@@ -32,19 +36,19 @@ class Share extends CI_Controller {
       if ($this->form_validation->run() == true) {
         // check whether file exists in the database
         $fileData['hash'] = $hash;
-				$fileData['password'] = sha1(sha1(sha1(strip_tags($this->input->post('password')))));
+        $fileData['password'] = sha1(sha1(sha1(strip_tags($this->input->post('password')))));
         $checkFile = $this->share_model->check_file($fileData);
-        if($checkFile != false){
-					// download file
-					$this->load->helper('download');
+        if ($checkFile != false) {
+          // download file
+          $this->load->helper('download');
           force_download($checkFile->link, NULL);
-        }else{
+        } else {
           $data['error_msg'] = 'Wrong password. Please try again.';
         }
       }
     }
-		// get file info
-		$data['file'] = $this->share_model->get_file($hash);
+    // get file info
+    $data['file'] = $this->share_model->get_file($hash);
 
     $data['title'] = 'Download File';
     $this->load->view('share/file', $data);
--- application/models/Share_model.php
+++ application/models/Share_model.php
@@ -1,27 +1,31 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class Share_model extends CI_Model{
-  function __construct() {
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+class Share_model extends CI_Model
+{
+  function __construct()
+  {
   }
 
-  function get_file($hash){
+  function get_file($hash)
+  {
     $query = $this->db->get_where('file', array('hash' => $hash));
-    if($query->num_rows() > 0) {
+    if ($query->num_rows() > 0) {
       return $query->row();
     }
 
     return false;
   }
 
-  function check_file($fileData){
-    $query = $this->db->from('file')->where(array('hash'=> $fileData['hash'], 'password' => $fileData['password']))->get();
-    if($query->num_rows() > 0) {
+  function check_file($fileData)
+  {
+    $query = $this->db->from('file')->where(array('hash' => $fileData['hash'], 'password' => $fileData['password']))->get();
+    if ($query->num_rows() > 0) {
       $file_info = $query->row();
 
       $downloaded = $file_info->downloaded + 1;
 
       $update = $this->db->where('id', $file_info->id)->update('file', array('downloaded' => $downloaded));
 
-      $file_info->link = './shared_file/'.$file_info->file;
+      $file_info->link = './shared_file/' . $file_info->file;
 
       return $file_info;
     }
